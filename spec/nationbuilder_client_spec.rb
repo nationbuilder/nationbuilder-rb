0ea07720f7383807a70f5052ab38f01dc4309cb7
Aaron Qian <aq1018@gmail.com>
2014-09-05 17:37:33 -0700

    fix specs

--- spec/nationbuilder_client_spec.rb
+++ spec/nationbuilder_client_spec.rb
@@ -12,12 +12,28 @@ describe NationBuilder::Client do
   describe '#endpoints' do
 
     it 'should contain all defined endpoints' do
-      client.endpoints.should eq([:basic_pages, :blog_posts, :blogs,
-                                   :calendars, :campaign_data, :contacts,
-                                   :contact_types, :donations, :events,
-                                   :imports, :lists, :memberships, :page_attachments,
-                                   :people, :people_tags, :sites,
-                                   :survey_responses, :surveys, :webhooks])
+      client.endpoints.sort.should eq([
+        :basic_pages,
+        :blog_posts,
+        :blogs,
+        :calendars,
+        :campaign_data,
+        :contact_types,
+        :contacts,
+        :donations,
+        :events,
+        :exports,
+        :imports,
+        :lists,
+        :memberships,
+        :page_attachments,
+        :people,
+        :people_tags,
+        :sites,
+        :survey_responses,
+        :surveys,
+        :webhooks
+      ])
     end
 
   end
