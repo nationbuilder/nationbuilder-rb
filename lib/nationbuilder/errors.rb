6842c04ad2d9d98da98d05f9c8cdef0e8a2e0a6c
Jeff Lee <jeffomatic@gmail.com>
2017-02-08 23:40:09 -0800

    lib/nationbuilder: include response object in errors
    This commit provides users with access to HTTP response objects
    when the NationBuilder client encounters an error. Among other
    things, this allows applications to respond intelligently to
    different status codes. For example, checking for a 401 status
    code can indicate to the host application that an access token
    has expired or is otherwise invalid.
    
    This commit is purely additive. Other than extending the error
    objects with a `response` accessor, there should be no other
    side effects.
    
    Affected classes:
    
    - ServerError
    - ClientError
    - RateLimitedError

--- lib/nationbuilder/errors.rb
+++ lib/nationbuilder/errors.rb
@@ -1,6 +1,15 @@
 module NationBuilder
   class BaseError < StandardError; end
-  class ServerError < BaseError; end
-  class ClientError < BaseError; end
+
+  class ResponseError < BaseError
+    attr_reader :response
+    def initialize(response = nil)
+      super(response ? response.body : nil)
+      @response = response
+    end
+  end
+
+  class ServerError < ResponseError; end
+  class ClientError < ResponseError; end
   class RateLimitedError < ClientError; end
 end
